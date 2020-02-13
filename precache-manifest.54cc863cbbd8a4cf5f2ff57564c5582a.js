e003150aa36b3d1a71cf4a803f72a5e44520adda
Anchello <bob.m@mail.ru>
2020-02-13 15:16:07 +0100

    Updates

---
+++ precache-manifest.54cc863cbbd8a4cf5f2ff57564c5582a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddd284f2ce7b5a9a11ff7c4767f2146",
+    "url": "/index.html"
+  },
+  {
+    "revision": "117378939a88637afd69",
+    "url": "/static/css/main.571995e7.chunk.css"
+  },
+  {
+    "revision": "c5da17985462166107e1",
+    "url": "/static/js/2.93ff344b.chunk.js"
+  },
+  {
+    "revision": "117378939a88637afd69",
+    "url": "/static/js/main.348b01b3.chunk.js"
+  },
+  {
+    "revision": "ec937081f4b2e98ce3b6",
+    "url": "/static/js/runtime-main.219139cd.js"
+  },
+  {
+    "revision": "6c1cbf764959c290d323fb8e7083505e",
+    "url": "/static/media/deck.6c1cbf76.png"
+  }
+]);
